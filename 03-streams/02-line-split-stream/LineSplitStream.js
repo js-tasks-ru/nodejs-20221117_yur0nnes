836cca605f168d6e5bc5d5e9eb36f3c4caeb76ef
Yuri <yur0n@yahoo.com>
2022-12-04 06:31:25 +0200

    4-module-1-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,20 +1,38 @@
 const stream = require('stream');
-const os = require('os');
 
 class LineSplitStream extends stream.Transform {
+  str = ''
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
-    chunk.forEach(el => {
-      console.log(el)
-    })
-    callback(null, chunk)
+    // chunk.toString().replaceAll(os.EOL, '-').split('').forEach(char => {
+    //   if (char == '-') {
+    //     this.push(this.str)
+    //     this.str = ''
+    //   } else this.str += char
+    // })
+    // ---------------------------------
+    // let text = chunk.toString()
+    // for (let i = 0; i < text.length; i++) {
+    //   if (text[i] == os.EOL) {
+    //     this.push(this.str)
+    //     this.str = ''
+    //   } else this.str += text[i]
+    // }
+    // ----------------------------------
+    for (let i = 0; i < chunk.length; i++) {
+        if (chunk[i] == 13 || chunk[i] == 10) {
+          this.push(this.str);
+          this.str = '';
+        } else this.str += String.fromCharCode(chunk[i]);
+    }
+    callback()
   }
 
   _flush(callback) {
-
+    callback(null, this.str)
   }
 }
 
